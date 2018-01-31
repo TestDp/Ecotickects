fd35fa67462d9bb2cfe9b42d3268e2dd3f666940
diegodpsoluciones <33739109+diegodpsoluciones@users.noreply.github.com>
2018-01-31 11:55:09 -0500

    Problema ciudades

--- resources/views/Evento/RegistrarAsistente.blade.php
+++ resources/views/Evento/RegistrarAsistente.blade.php
@@ -199,4 +199,5 @@
         </div>
     </div>
     <script src="{{ asset('js/Evento/eventos.js') }}"></script>
+    <script src="{{ asset('js/Plugins/Jquery/jquery-3.1.1.js') }}"></script>
 @endsection
