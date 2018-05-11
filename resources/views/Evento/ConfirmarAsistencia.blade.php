0a3d15932d164d2efa1c154a7f12c35b0643c6e5
diegodpsoluciones <33739109+diegodpsoluciones@users.noreply.github.com>
2018-05-10 21:20:04 -0500

    campo cedula

--- resources/views/Evento/ConfirmarAsistencia.blade.php
+++ resources/views/Evento/ConfirmarAsistencia.blade.php
@@ -17,6 +17,16 @@
 						<h3>¿Contamos con tu asistencia para vivir la experiencia Lovers Festival?. </h3>
 						<form action="">
 						<div class="row">
+						<div class="col-md-3">
+						</div>
+							<div class="col-md-6">
+								Identificación
+								<input id="Identificacion" name="Identificacion" type="number" class="form-control" onchange="BuscarAsistente()"/>
+							</div>
+						<div class="col-md-3">
+						</div>	
+						</div>	
+						<div class="row">
 								<div class="col-md-6">
 															<b style="font-family: sans-serif; font-size: 40px;" >
 																<input type="radio" name="confirmarAsistencia" value="si" id="si" /> SI 
