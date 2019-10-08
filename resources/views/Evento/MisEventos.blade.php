4b678dcdfe2669441c8b00e0d7b6fc0794985452
TestDp <Cristianmg13@hotmail.com>
2019-10-07 22:27:20 -0500

    pestañas de eventos próximos y pasados

--- resources/views/Evento/MisEventos.blade.php
+++ resources/views/Evento/MisEventos.blade.php
@@ -20,108 +20,127 @@
                                 </div>
                             </div>
                         @endif
-						<div style="overflow-x:auto;">
-						<table style="border-collapse: collapse !important; border-spacing: 0 !important; width: 100% !important;" id="TablaListaEventos" class="table table-bordered">
-                            <thead>
-                            <tr >
-                                <th >
-                                    Id
-                                </th>
-                                <th >
-                                    Nombre
-                                </th>
-                                <th >
-                                    Lugar
-                                </th>
-                                <th >
-                                    Ciudad
-                                </th>
-                                <th >
-                                    Departamento
-                                </th>
-                                <th >
-                                    Fecha del Evento
-                                </th>
-                                <th >
-                                    Fecha Inicial de registro
-                                </th>
-                                <th >
-                                    Fecha Final de registro
-                                </th>
-                                @if(Auth::user()->buscarRecurso('ListaAsistentes'))
-                                    <th >
-                                        Usuarios Registrados
-                                    </th>
-                                @endif
-                                @if(Auth::user()->buscarRecurso('Estadisticas'))
-                                    <th>
-                                        Estadísticas
-                                    </th>
-                                @endif
-                                @if(Auth::user()->buscarRecurso('LecturaQR'))
-                                    <th>
-                                        Leer QR
-                                    </th>
-                                @endif
-                                @if(Auth::user()->buscarRecurso('EditarEvento'))
-                                    <th>
 
-                                    </th>
-                                @endif
-                            </tr>
-                            </thead>
-                            <tbody >
-                            @foreach($ListaEventos["eventos"] as $evento)
-                                <tr>
-                                    <td >
-                                        {{ $evento->id }}
-                                    </td>
-                                    <td >
-                                        {{ $evento->Nombre_Evento }}
-                                    </td>
-                                    <td >
-                                        {{ $evento->Lugar_Evento }}
-                                    </td>
-                                    <td >
-                                        {{ $evento->ciudad->Nombre_Ciudad }}
-                                    </td>
-                                    <td>
-                                        {{ $evento->ciudad->departamento->Nombre_Departamento }}
-                                    </td>
-                                    <td >
-                                        {{ $evento->Fecha_Evento }}
-                                    </td>
-                                    <td >
-                                        {{ $evento->Fecha_Inicial_Registro }}
-                                    </td>
-                                    <td>
-                                        {{ $evento->Fecha_Final_Registro }}
-                                    </td>
-                                    @if(Auth::user()->buscarRecurso('ListaAsistentes'))
-                                        <td>
-                                                 <a class="btn btn-blue ripple trial-button" href="{{ url('/ListaAsistentes',['idEvento' => $evento->id ]) }}">ver</a>
-                                        </td>
-                                    @endif
-                                    @if(Auth::user()->buscarRecurso('Estadisticas'))
-                                        <td>
-                                                <a class="btn btn-blue ripple trial-button" href="{{ url('/Estadisticas',['idEvento' => $evento->id ]) }}">ver</a>
-                                        </td>
-                                    @endif
-                                    @if(Auth::user()->buscarRecurso('LecturaQR'))
-                                        <td>
-                                                <a class="btn btn-blue ripple trial-button" href="{{ url('/LecturaQR',['idEvento' => $evento->id ]) }}">Leer QR</a>
-                                        </td>
-                                    @endif
-                                    @if(Auth::user()->buscarRecurso('EditarEvento'))
-                                        <td>
-                                                <a class="btn btn-blue ripple trial-button" href="{{ url('/EditarEvento',['idEvento' => $evento->id ]) }}">Editar</a>
-                                        </td>
-                                    @endif
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-						</div>
+                            <ul class="nav nav-tabs" >
+                                <li class="active">
+                                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#EventosProximo" role="tab" aria-controls="home"
+                                       aria-selected="true">Eventos Proximos</a>
+                                </li>
+                                <li>
+                                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#EventosPasados" role="tab" aria-controls="profile"
+                                       aria-selected="false">Eventos pasados</a>
+                                </li>
+                            </ul>
+                            <div class="tab-content" id="myTabContent">
+                                <div class="tab-pane fade show active" id="EventosProximo" role="tabpanel" aria-labelledby="home-tab">
+                                    <div style="overflow-x:auto;">
+                                        <table style="border-collapse: collapse !important; border-spacing: 0 !important; width: 100% !important;" id="TablaListaEventos" class="table table-bordered">
+                                            <thead>
+                                            <tr >
+                                                <th >
+                                                    Id
+                                                </th>
+                                                <th >
+                                                    Nombre
+                                                </th>
+                                                <th >
+                                                    Lugar
+                                                </th>
+                                                <th >
+                                                    Ciudad
+                                                </th>
+                                                <th >
+                                                    Departamento
+                                                </th>
+                                                <th >
+                                                    Fecha del Evento
+                                                </th>
+                                                <th >
+                                                    Fecha Inicial de registro
+                                                </th>
+                                                <th >
+                                                    Fecha Final de registro
+                                                </th>
+                                                @if(Auth::user()->buscarRecurso('ListaAsistentes'))
+                                                    <th >
+                                                        Usuarios Registrados
+                                                    </th>
+                                                @endif
+                                                @if(Auth::user()->buscarRecurso('Estadisticas'))
+                                                    <th>
+                                                        Estadísticas
+                                                    </th>
+                                                @endif
+                                                @if(Auth::user()->buscarRecurso('LecturaQR'))
+                                                    <th>
+                                                        Leer QR
+                                                    </th>
+                                                @endif
+                                                @if(Auth::user()->buscarRecurso('EditarEvento'))
+                                                    <th>
+
+                                                    </th>
+                                                @endif
+                                            </tr>
+                                            </thead>
+                                            <tbody >
+                                            @foreach($ListaEventos["eventos"] as $evento)
+                                                <tr>
+                                                    <td >
+                                                        {{ $evento->id }}
+                                                    </td>
+                                                    <td >
+                                                        {{ $evento->Nombre_Evento }}
+                                                    </td>
+                                                    <td >
+                                                        {{ $evento->Lugar_Evento }}
+                                                    </td>
+                                                    <td >
+                                                        {{ $evento->ciudad->Nombre_Ciudad }}
+                                                    </td>
+                                                    <td>
+                                                        {{ $evento->ciudad->departamento->Nombre_Departamento }}
+                                                    </td>
+                                                    <td >
+                                                        {{ $evento->Fecha_Evento }}
+                                                    </td>
+                                                    <td >
+                                                        {{ $evento->Fecha_Inicial_Registro }}
+                                                    </td>
+                                                    <td>
+                                                        {{ $evento->Fecha_Final_Registro }}
+                                                    </td>
+                                                    @if(Auth::user()->buscarRecurso('ListaAsistentes'))
+                                                        <td>
+                                                            <a class="btn btn-blue ripple trial-button" href="{{ url('/ListaAsistentes',['idEvento' => $evento->id ]) }}">ver</a>
+                                                        </td>
+                                                    @endif
+                                                    @if(Auth::user()->buscarRecurso('Estadisticas'))
+                                                        <td>
+                                                            <a class="btn btn-blue ripple trial-button" href="{{ url('/Estadisticas',['idEvento' => $evento->id ]) }}">ver</a>
+                                                        </td>
+                                                    @endif
+                                                    @if(Auth::user()->buscarRecurso('LecturaQR'))
+                                                        <td>
+                                                            <a class="btn btn-blue ripple trial-button" href="{{ url('/LecturaQR',['idEvento' => $evento->id ]) }}">Leer QR</a>
+                                                        </td>
+                                                    @endif
+                                                    @if(Auth::user()->buscarRecurso('EditarEvento'))
+                                                        <td>
+                                                            <a class="btn btn-blue ripple trial-button" href="{{ url('/EditarEvento',['idEvento' => $evento->id ]) }}">Editar</a>
+                                                        </td>
+                                                    @endif
+                                                </tr>
+                                            @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <div class="tab-pane fade" id="EventosPasados" role="tabpanel" aria-labelledby="profile-tab">
+                                    <!-- poner la tabla de eventos pasados-->
+                                </div>
+                            </div>
+
                     </div>
                 </div>
             </div>
