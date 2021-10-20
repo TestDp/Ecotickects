99667c387056f5f063917bd70cf9eb2058feb4c2
Administrador <Cristianmg13@hotmail.com>
2021-10-20 17:25:56 -0500

    CMAG:Se ajusta el AsistentesController

--- app/Http/Controllers/Evento/AsistentesController.php
+++ app/Http/Controllers/Evento/AsistentesController.php
@@ -315,8 +315,6 @@ class AsistentesController extends Controller
             $verficarFirma = $this->asistenteServicio->validarFirmaPago($merchantId, $referenciaVenta, $valor, $moneda, $estadoVenta, $firmaVenta);
             //$verificarfirma:1 para la validacion de la firma es correcta
             //$verificarfirma:0 para la validacion de la firma es incorrecta
-            $verficarFirma = 1;
-            $estadoVenta = 4;
             if ($estadoVenta == 4 && $verficarFirma == 1) {
                 $this->asistenteServicio->ActualizarPinBusquedaCorreo($formRegistro->email_buyer);
                 $listaAsistentesXEventosPines = $this->asistenteServicio->crearBoletas($referenciaVenta, $estadoVenta, $medioPago);
