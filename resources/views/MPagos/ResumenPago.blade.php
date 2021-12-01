36f272473b2fd43ef40f452ccb6f90680cc77388
diegodpsoluciones <33739109+diegodpsoluciones@users.noreply.github.com>
2021-12-01 14:29:18 -0500

    solución pgos promotor

--- resources/views/MPagos/ResumenPago.blade.php
+++ resources/views/MPagos/ResumenPago.blade.php
@@ -51,29 +51,29 @@
 											<div class="row">
 												<div class="col-md-3">
 													<a id="aVisa" name="aVisa" class="navbar-brand"  onclick="AgregarVlrPagoTC('VISA')" style="cursor:pointer;">
-														<img style="height: auto !important;" src="../img/icono_visa.png" data-active-url="../img/icono_visa.png" alt=""  width="100%" height="100%"/>
+														<img style="height: auto !important;" src="{{ asset('img/icono_visa.png')}}" data-active-url="{{ asset('/img/icono_visa.png')}}" alt=""  width="100%" height="100%"/>
 													</a>
 												</div>
 												<div class="col-md-3">
 													<a id="aMasterCard" name="aMasterCard" class="navbar-brand" onclick="AgregarVlrPagoTC('MASTERCARD')" style="cursor:pointer;">
-														<img style="height: auto !important;" src="../img/icono_mastercard.png" data-active-url="../img/icono_visa.png" alt=""  width="100%" height="100%"/>
+														<img style="height: auto !important;" src="{{ asset('img/icono_mastercard.png')}}" data-active-url="{{ asset('img/icono_visa.png')}}" alt=""  width="100%" height="100%"/>
 													</a>
 												</div>
 												<div class="col-md-3">
 													<a id="aAmex" name="aAmex" class="navbar-brand" onclick="AgregarVlrPagoTC('AMEX')" style="cursor:pointer;">
-														<img style="height: auto !important;" src="../img/icono_amex.png" data-active-url="../img/icono_visa.png" alt="" width="100%" height="100%"/>
+														<img style="height: auto !important;" src="{{ asset('img/icono_amex.png')}}" data-active-url="{{ asset('img/icono_visa.png')}}" alt="" width="100%" height="100%"/>
 													</a>
 												</div>
 											</div>
 											<div class="row">
 												<div class="col-md-3">
 													<a id="aDiners" name="aDiners" class="navbar-brand" onclick="AgregarVlrPagoTC('DINERS')" style="cursor:pointer;">
-														<img style="height: auto !important;" src="../img/icono_diners.png" data-active-url="../img/icono_visa.png" alt="" width="100%" height="100%"/>
+														<img style="height: auto !important;" src="{{ asset('img/icono_diners.png')}}" data-active-url="{{ asset('img/icono_visa.png')}}" alt="" width="100%" height="100%"/>
 													</a>
 												</div>
 												<div class="col-md-3">
 													<a id="aCodensa" name="aCodensa" class="navbar-brand" onclick="AgregarVlrPagoTC('CODENSA')" style="cursor:pointer;">
-														<img style="height: auto !important;" src="../img/icono_codensa.jpg" data-active-url="../img/icono_visa.png" alt="" width="100%" height="100%"/>
+														<img style="height: auto !important;" src="{{ asset('img/icono_codensa.jpg')}}" data-active-url="{{ asset('img/icono_visa.png')}}" alt="" width="100%" height="100%"/>
 													</a>
 												</div>
 											</div>
@@ -84,7 +84,7 @@
 											<div class="row">
 												<div class="col-md-4">
 													<a id="aPSE" name="aPSE" class="navbar-brand" onclick="cargarFormularioPagoPSE()" style="cursor:pointer;">
-														<img style="height: auto !important;" src="../img/icono_pse.jpg" data-active-url="../img/icono_visa.png" alt="" width="100%" height="100%"/>
+														<img style="height: auto !important;" src="{{ asset('img/icono_pse.jpg')}}" data-active-url="{{ asset('img/icono_visa.png')}}" alt="" width="100%" height="100%"/>
 													</a>
 												</div>
 												<div class="col-md-8">
